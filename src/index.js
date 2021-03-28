5b823b7481e2433729618be36be6efea86d43439
brunadossantos-tech <74012238+brunadossantos-tech@users.noreply.github.com>
2021-03-28 18:57:33 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -113,7 +113,7 @@ function showForecast(response){
         forecast = response.data.list[index];
 
         forecastElement.innerHTML += `
-           <div class="col-sm-2">
+           <div class="col-md-2">
            <h3>
             ${formatHours(forecast.dt* 1000)}
             </h3>
